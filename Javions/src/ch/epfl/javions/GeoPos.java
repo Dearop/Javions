b20e1947e5b4cf7e2f2411bd134b67fb72d521cc
HenriAntal <henrimaxim@gmail.com>
2023-02-20 19:31:01 +0100

    added 2 functions in GeoPos and changed one value in isValidLatitude
    added longitude and latitude to GeoPos, check if return values are okay, we can see tomorrow :)

--- Javions/src/ch/epfl/javions/GeoPos.java
+++ Javions/src/ch/epfl/javions/GeoPos.java
@@ -1,16 +1,62 @@
 package ch.epfl.javions;
 
+
+import static ch.epfl.javions.Units.*;
+
+/**
+ * This record represents GeoPos
+ *
+ * @param longitudeT32
+ * @param latitudeT32
+ * @author Henri Antal (339444)
+ * @author Paul Quesnot (TODO Nr.)
+ */
 public record GeoPos(int longitudeT32, int latitudeT32) {
-    /*Checks if a given latitude is possible :
-        by checking the lower bound, if the latitude is lower than the lower bound, it is not valid
-        by checking the upper bound, if the latitude is higher than the lower bound, it is not valid
-        if the two are false than the latitude must be valid
+
+    /**
+     * This compact constructor checks that the given arguments are in bound, if they are not then
+     * this constructor will throw an exception
+     *
+     * @param longitudeT32 is the longitude written in 32 bits
+     * @param latitudeT32 is the latitude written in 32 bits
      */
-    public static boolean isValidLatitudeT32(int latitudeT32){
-        if (latitudeT32 < Math.scalb(-1,-30)) return false;
+    public GeoPos { // constructeur compact
+        //TODO is this function necessary?? I think so but I am not sure (3.7) (It would check longitude)
+        // if(!isValidLatitudeT32(longitudeT32)) throw new IllegalArgumentException();
+        if(!isValidLatitudeT32(latitudeT32)) throw new IllegalArgumentException(); //checks if latitude is inbound
+
+    }
+
+    /**
+     * Checks if a given latitude is possible :
+     * by checking the lower bound, if the latitude is lower than the lower bound, it is not valid
+     * by checking the upper bound, if the latitude is higher than the lower bound, it is not valid
+     * if the two are false than the latitude must be valid
+     *
+     * @param latitudeT32
+     * @return
+     */
+    public static boolean isValidLatitudeT32(int latitudeT32) {
+        if (latitudeT32 < Math.scalb(-1, 30)) return false; //changed the minus here (before it was (-1, -30)
         if (latitudeT32 > Math.scalb(1, 30)) return false;
         return true;
     }
 
+    /**
+     * This function lets you convert the longitude from the 32 bits information into radians
+     * @return double Radians
+     */
+    public double longitude(){
+        return convert(longitudeT32, Angle.T32, Angle.RADIAN);
+    }
 
+    /**
+     * This function lets you convert the latitude from the 32 bits information into radians
+     * @return double Radians
+     */
+    public double latitude(){
+        return convert(latitudeT32, Angle.T32, Angle.RADIAN);
+    }
+
+    //TODO toString function in (3.7)
 }
--- Javions/src/ch/epfl/javions/Units.java
+++ Javions/src/ch/epfl/javions/Units.java
@@ -2,50 +2,73 @@
 
 // this class contains CONSTANTS that will be used throughout the project, they can't be changed or instantiated
 public final class Units {
-    private Units(){}
+    private Units() {
+    }
+
     public static final double CENTI = 1e-2;
     public static final double KILO = 1e-3;
 
-    public static class Angle{
+    public static class Angle {
         public static final double RADIAN = 1;
-        public static final double TURN = 2*Math.PI;
-        public static final double DEGREE = TURN/360;
-        public static final double T32 = TURN/(2^32);
-        private Angle(){}
+        public static final double TURN = 2 * Math.PI;
+        public static final double DEGREE = TURN / 360;
+        public static final double T32 = TURN / (2 ^ 32);
+
+        private Angle() {
+        }
     }
 
-    public static class Length{
+    public static class Length {
         public static final double METER = 1;
-        public static final double CENTIMETER = CENTI*METER;
-        public static final double KILOMETER = KILO*METER;
-        public static final double INCH = 2.54*CENTIMETER;
-        public static final double FOOT = 12*INCH;
+        public static final double CENTIMETER = CENTI * METER;
+        public static final double KILOMETER = KILO * METER;
+        public static final double INCH = 2.54 * CENTIMETER;
+        public static final double FOOT = 12 * INCH;
         public static final double NAUTICAL_MILE = 1852;
-        private Length(){}
+
+        private Length() {
+        }
     }
 
-    public static class Time{
+    public static class Time {
         public static final double SECOND = 1;
         public static final double MINUTE = 60;
-        public static final double HOUR = 60*MINUTE;
-        private Time(){}
+        public static final double HOUR = 60 * MINUTE;
+
+        private Time() {
+        }
     }
 
-    public static class Speed{
+    public static class Speed {
         public static final double KNOT = 1.944;
         public static final double KILOMETER_PER_HOUR = 3.6;
-        private Speed(){}
+
+        private Speed() {
+        }
     }
 
-    public static double convert(double value,double fromUnit,double toUnit){
-        return value * (fromUnit/toUnit);
+
+    /**
+     * these next three functions are defined in (3.4)
+     * this function converts from one unit system to another unit system
+     * for example convert(2, FOOT, CENTI) gives you the value 60.96 so we change from 2 foot to 60.96cm
+     *
+     * @param value    a
+     * @param fromUnit b
+     * @param toUnit   c
+     * @return
+     */
+    public static double convert(double value, double fromUnit, double toUnit) {
+        return value * (fromUnit / toUnit);
     }
 
-    public static double convertFrom(double value, double fromUnit){
-        return value*fromUnit;
+    // this function gives you the calculation from the given unit to the base unit
+    public static double convertFrom(double value, double fromUnit) {
+        return value * fromUnit;
     }
 
-    public static double convertTo(double value, double toUnit){
-        return value/toUnit;
+    // this function gives you the calculation from the base unit to the input unit that we want to reach.
+    public static double convertTo(double value, double toUnit) {
+        return value / toUnit;
     }
 }
