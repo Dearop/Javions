66e52c50dd923a7bc3dfc7c28272f0d79947a2c7
Dearop <rocamar564p@gmail.com>
2023-02-20 17:09:36 +0100

    Finished math2

--- Javions/src/ch/epfl/javions/Math2.java
+++ Javions/src/ch/epfl/javions/Math2.java
@@ -3,14 +3,20 @@ package ch.epfl.javions;
 public final class Math2 {
     private Math2(){}
 
+    /*  throws IllegalArgumentException if min bigger than max,
+        returns the minimum if v is smaller than min
+        returns the maximum if v is bigger than max
+        returns v otherwise (3.5)
+     */
     public static int clamp(int min, int v, int max){
         if (min > max) throw new IllegalArgumentException();
-        if (v < min) return min;
-        if (v > max) return max;
+        else if (v < min) return min;
+        else if (v > max) return max;
         return v;
     }
 
+    // returns arsinh of the x value (3.5)
     public static double asinh(double x){
-
+        return Math.log(x+Math.sqrt(1+Math.pow(x,2)));
     }
 }
