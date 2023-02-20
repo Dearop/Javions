411ef00d75870f75ac9a23eefc6be7bca9e16b55
Dearop <rocamar564p@gmail.com>
2023-02-20 16:32:42 +0100

    Units class variables done

--- Javions/src/ch/epfl/javions/Units.java
+++ Javions/src/ch/epfl/javions/Units.java
@@ -1,7 +1,7 @@
 package ch.epfl.javions;
 
 public final class Units {
-    public static final double CENTI = 10^-3;
+    public static final double CENTI = 10^-2;
     public static final double KILO = 10^3;
 
     public static class Angle{
@@ -13,20 +13,25 @@ public final class Units {
     }
 
     public static class Length{
-        public static final double METER;
-        public static final double CENTIMETER;
-        public static final double KILOMETER;
-        public static final double INCH;
-        public static final double FOOT;
-        public static final double 
+        public static final double METER = 1;
+        public static final double CENTIMETER = 10^-2;
+        public static final double KILOMETER = 10^3;
+        public static final double INCH = 2.54*CENTIMETER;
+        public static final double FOOT = 12*INCH;
+        public static final double NAUTICAL_MILE = 1852;
         private Length(){}
     }
 
     public static class Time{
+        public static final double SECOND = 1;
+        public static final double MINUTE = 60;
+        public static final double HOUR = 60*MINUTE;
         private Time(){}
     }
 
     public static class Speed{
+        public static final double KNOT = 1/1.944;
+        public static final double KILOMETER_PER_HOUR = 1/3.6;
         private Speed(){}
     }
 }
