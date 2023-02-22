2ac26bc331d83c4f1093b04bfc21927afc3af640
HenriAntal <henrimaxim@gmail.com>
2023-02-22 20:34:34 +0100

    Update ByteString.java
    only changed a small ting in byteAt where 0 is also included and added links and comments

--- Javions/src/ch/epfl/javions/ByteString.java
+++ Javions/src/ch/epfl/javions/ByteString.java
@@ -1,6 +1,7 @@
 package ch.epfl.javions;
 
 import java.util.HexFormat;
+import java.util.Objects;
 
 /**
  * @author Henri Antal (339444)
@@ -18,7 +19,12 @@ public final class ByteString {
         byte[] nonSigne = new byte[bytes.length];
         for(int i = 0; i < bytes.length; ++i){
             nonSigne[i] = (byte) Math.abs(bytes[i]);
-        }
+        } /**
+         TODO i think this is wrong how can one just use abs(of each value in bytes because they are positive
+         anyways (0-9,A-F) but if index 7 is 1 then java makes it negative so we need to change that, here is a
+         link for it: https://stackoverflow.com/questions/4266756/can-we-make-unsigned-byte-in-java use this in
+         byteAt, i think this would fix it, also our max would then be 256 and not 127
+        */
         finalBytes = nonSigne.clone();
     }
     public static ByteString ofHexadecimalString(String hexString){
@@ -37,15 +43,22 @@ public final class ByteString {
     }
 
     public int byteAt(int index){
-        if (index <= 0 || index > 7) throw new IndexOutOfBoundsException();
+        // changed index <= 0 because index 0 can be inside the byte
+        if (index < 0 || index > 7) throw new IndexOutOfBoundsException();
+        if (finalBytes[7] == 1){
+            //Todo how do i change the whole value of finalBytes, also 0xFF is from link above
+            //finalBytes &= 0xFF;
+        }
         return finalBytes[index];
     }
 
     public long bytesInRange(int fromIndex, int toIndex){
-        if(toIndex-fromIndex < 0 || toIndex-fromIndex > finalBytes.length) throw new IndexOutOfBoundsException();
-        if(toIndex-fromIndex >= 4) throw new IllegalArgumentException();
-        return 0;
+        // need to use checkFromToIndex
+        Objects.checkFromToIndex(fromIndex,toIndex,8);
+        //if(toIndex-fromIndex < 0 || toIndex-fromIndex > finalBytes.length) throw new IndexOutOfBoundsException();
+        //if(toIndex-fromIndex >= 4) throw new IllegalArgumentException();
+        return 121212121;
     }
 
-    public boolean equals()
+    //public boolean equals()
 }
