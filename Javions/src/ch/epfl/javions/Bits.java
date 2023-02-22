e240b2c9d86732829261b1ecfc32d222396e1acf
HenriAntal <henrimaxim@gmail.com>
2023-02-22 17:26:48 +0100

    Update Bits.java
    Changed the bits class a bit, also for testBit changed the line to see if first bit is one, with using modulo 2 on extractvalue

--- Javions/src/ch/epfl/javions/Bits.java
+++ Javions/src/ch/epfl/javions/Bits.java
@@ -1,7 +1,8 @@
 package ch.epfl.javions;
 
 public class Bits {
-    private Bits(){}
+    private Bits() {
+    }
 
     /**
      * In this function we extract a number out of a given value. The value is given as a binary Number.
@@ -13,16 +14,14 @@ public class Bits {
      * @throws IllegalArgumentException if size is strictly smaller than 0 or start + size is bigger or equal to 64
      * @return
      */
-    public int extractUInt(long value, int start, int size){
+    public int extractUInt(long value, int start, int size) {
 
         if (size < 0 || size > 32) throw new IllegalArgumentException();
         else if (start < 0 || start + size > 63) throw new IndexOutOfBoundsException();
 
-        int extractedValue = (int) value >>>(start);
-        int changedValueSize = 0;
-        for(int i = 0; i < size; ++i){
-            changedValueSize += Math.pow(2,i);
-        }
+        int extractedValue = (int) value >>> (start);
+        int changedValueSize = (int) (Math.pow(2, size) - 1);  //if size for example is 5 we get 31 which would be written
+        // as ..011111
         extractedValue &= changedValueSize;
         return extractedValue;
     }
@@ -38,10 +37,10 @@ public class Bits {
      * @throws IndexOutOfBoundsException if index is smaller than 0 or index is strictly bigger than 63
      * @return
      */
-    public static boolean testBit(long value, int index){
+    public static boolean testBit(long value, int index) {
         if (index < 0 || index > 63) throw new IndexOutOfBoundsException();
         int extractedValue = (int) value >>> index;
-        if((extractedValue &= 1) == 1) return true;
+        if ((extractedValue %= 2) == 1) return true;
         return false;
     }
 }
