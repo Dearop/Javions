861f45b7d287dd5d2fcb257f1b356f6e5434bfed
Dearop <rocamar564p@gmail.com>
2023-02-22 10:47:51 +0100

    Bits is done - 22.02.2023

--- Javions/src/ch/epfl/javions/Bits.java
+++ Javions/src/ch/epfl/javions/Bits.java
@@ -13,12 +13,11 @@ public class Bits {
      * @return
      */
     public int extractUInt(long value, int start, int size){
+
         if (size < 0) throw new IllegalArgumentException();
         else if (size > 32) throw new IllegalArgumentException();
-        //@Todo there is no way that this is correct, for example you have size = 5 and start = 10
-        //that would give an exception... i propose that it is (start >= 0) && (start + size < 64)
-        // I agree
         else if (!(start >= 0) || !(start + size < 64)) throw new IndexOutOfBoundsException();
+
         int extractedValue = (int) value >>>(start);
         int changedValueSize = 0;
         for(int i = 0; i < size; ++i){
@@ -39,20 +38,9 @@ public class Bits {
      * @return
      */
     public static boolean testBit(long value, int index){
-        if (index <= 0 ^ index > 64) throw new IndexOutOfBoundsException();
-        String sValue = Long.toString(value);
-        if (sValue.charAt(sValue.length()-index) == 1) return true;
-        /**
-         * I don't understand this method do they want us to give a return true iff the given value at the given index
-         * is equal to one??? but give a OutOfBoundsException if it is between 0-63 ?? /TODO (3.9)
-         *
-         * Basically they want you to throw the Exception if index is not in [0,64[,
-         * if the number at the index of value is equal to one, then you return true, if it isn't
-         * then the function sends back false.
-         *
-         * QUICK DISCLAIMER, IT'S WRITTEN UNDERNEATH THAT BITS START ON THE RIGHT, NOT THE LEFT LIKE STRINGS DO
-         * This is why I added sValue.length()
-         */
+        if (index < 0 || index > 63) throw new IndexOutOfBoundsException();
+        int extractedValue = (int) value >>> index;
+        if((extractedValue &= 1) == 1) return true;
         return false;
     }
 }
