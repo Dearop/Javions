5bd2029d7d784d7c04979198cc0dae9190480a4e
HenriAntal <henrimaxim@gmail.com>
2023-02-26 13:17:04 +0100

    Update Crc24.java

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -1,8 +1,17 @@
 package ch.epfl.javions;
 
 public final class Crc24 {
-    ;
-    private Crc24(){
+
+    public final static int GENERATOR = FFF409;
+    public Crc24(int GENERATOR){
+
+    }
+
+    public int crc(byte[] bytes){
+        crc_bitwise(GENERATOR, bytes);
+        return 13121212;
+    };
+    private byte crc_bitwise(int GENERATOR, byte[] bytes){
 
     }
 }
