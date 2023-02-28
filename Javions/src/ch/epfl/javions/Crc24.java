28e4f2dbd2de6de49bc419525928e61454d1f9c2
Dearop <rocamar564p@gmail.com>
2023-02-28 09:15:43 +0100

    crc is shit

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -4,36 +4,43 @@ public final class Crc24 {
 
     public final static int GENERATOR = 0xFFF409;
     private final int generator;
+    private int[] crc_b = new int[256];
     public Crc24(int generator){
         this.generator = Bits.extractUInt(generator, 0, 24);
+        for(int i = 0; i < 256; ++i){
+            this.crc_b[i] = crc_bitwise(generator, new byte[]{(byte) i});
+        }
+
     }
 
     public int crc(byte[] bytes){
-        return Bits.extractUInt(crc_bitwise(generator, bytes), 0,24);
+        return 0;
     }
-    private static int crc_bitwise(int generator, byte[] bytes){
+    public static int crc_bitwise(int generator, byte[] bytes){
         int crc = 0;
-        int[] table = {0, generator};
+        byte[] zeroes = new byte[3];
+        int[] table = {0,generator};
         //traitement du message
         for(int o = 0; o < bytes.length; o++){
             byte usedByte = bytes[o];
-            crc = ((crc << 8) | usedByte) ^ table[(crc >> countBits(crc)-1)];
+            for (int j = 8; j > 0; --j) {
+                int bit = Bits.extractUInt(usedByte, j, 1);
+                crc = ((crc << 1) | bit) ^ table[Bits.extractUInt(crc, 23, 1)];
+            }
+            System.out.print(2);
+        }
+        for (int i = 0; i < 24; ++i){
+            crc = (crc << 1) ^ table[Bits.extractUInt(crc, 23, 1)];
         }
         return crc;
-
-    }
-
-    private static int countBits(int number) {
-
-        return (int)(Math.log(2)/Math.log(number))+1;
     }
 
     private static int indexFinder(int crc){
         int index = 0;
-        while(crc != 1){
+        do {
             crc >>= 1;
             ++index;
-        }
+        } while (crc != 1);
         return index-1;
     }
 
--- Javions/test/ch/epfl/javions/CRC24Test.java
+++ Javions/test/ch/epfl/javions/CRC24Test.java
@@ -12,11 +12,12 @@ public class CRC24Test {
         String cS1 = "035DB8";
         int c = Integer.parseInt(cS1, 16); // == 0x035DB8
 
-        byte[] mAndC = HexFormat.of().parseHex(mS1 + cS1);
-        assertEquals(0, crc24.crc(mAndC));
-
         byte[] mOnly = HexFormat.of().parseHex(mS1);
-        assertEquals(c, crc24.crc(mOnly));
+        assertEquals(c, crc24.crc_bitwise(Crc24.GENERATOR, mOnly));
+
+        byte[] mAndC = HexFormat.of().parseHex(mS1 + cS1);
+        assertEquals(0, crc24.crc_bitwise(Crc24.GENERATOR,mAndC));
+
 
     }
 
