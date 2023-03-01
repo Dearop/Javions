108a15d7f0f7aa39f2c457edd27db961c712d96a
HenriAntal <henrimaxim@gmail.com>
2023-03-01 16:16:02 +0100

    Update AircraftDatabase.java

--- Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
+++ Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
@@ -1,22 +1,55 @@
 package ch.epfl.javions.aircraft;
+
 import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-/**
- *  @author Henri Antal (339444)
- *  @author Paul Quesnot (347572)
- */
 public final class AircraftDatabase {
     private String fileName;
-    public AircraftDatabase(String fileName){
-        if(fileName == null) throw new NullPointerException();
+
+    public AircraftDatabase(String fileName) {
+        if (fileName == null) throw new NullPointerException();
         this.fileName = fileName;
     }
-/*    public AircraftData get(IcaoAddress address) throws IOException {
+
+    public AircraftData get(IcaoAddress address) throws IOException {
         String aircraft = getClass().getResource("/aircraft.zip").getFile();
+        String stringLineFiltered = getStringLine(aircraft);
+        int index;
+
+        // up to here code works duh
+        String addressString = String.valueOf(address);
+        if (stringLineFiltered.startsWith(addressString)) {
+            //finding the index of where the addressString is located
+            index = stringLineFiltered.indexOf(addressString);
+
+            // splits the string into an array for every line
+            String[] lines = stringLineFiltered.split(System.getProperty("line.separator"));
+        }
+
+
+        // in here you need to go through the stringLineFiltered and find the address given
+        /**
+         *Example of a few lines in stringLineFiltered:
+         *
+         * E48414,PR-GGU,B738,BOEING 737-800,L2J,M
+         * E48614,PR-GUF,B738,BOEING 737-800,L2J,M
+         * E48714,PP-JMJ,C750,CESSNA 750 Citation 10,L2J,M
+         * E48A14,PT-STK,C525,CESSNA 525 CitationJet,L2J,L
+         *
+         * the first value is the ICAO-address
+         */
+
+
+        //if(address == a certain icaoaddress)
+
+        return // has to be the address file called by icao;
+    }
+
+    private static String getStringLine(String aircraft){
+
         try (ZipFile zipFileUsed = new ZipFile(aircraft);
              InputStream stream = zipFileUsed.getInputStream(zipFileUsed.getEntry("14.csv"));
              Reader reader = new InputStreamReader(stream, UTF_8);
@@ -24,15 +57,10 @@ public final class AircraftDatabase {
             String stringLineFiltered = "";
             while ((stringLineFiltered = buffer.readLine()) != null)
                 System.out.println(stringLineFiltered);
-            while(!address.equals(stringLineFiltered)){
-                buffer.readLine();// like thisthis?
-            }
-        } // up to here code works duh
-        // in here you need to go through the stringLineFiltered and find the address given
-        //if(address == a certain icaoaddress)
-
-        return ;// has to be the address file called by icao;
+            return stringLineFiltered;
+        } catch (IOException e) {
+            throw new RuntimeException(e +" zipFileError");
+        }
     }
 
- */
 }
