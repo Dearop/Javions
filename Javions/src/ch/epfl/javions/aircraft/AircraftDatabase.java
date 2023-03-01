69d34fc1aeffbda6f46c9c54c28b3612252e9071
HenriAntal <henrimaxim@gmail.com>
2023-03-01 17:17:21 +0100

    Update AircraftDatabase.java
    almost perfect :+1:

--- Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
+++ Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class AircraftDatabase {
@@ -16,39 +17,42 @@ public final class AircraftDatabase {
 
     public AircraftData get(IcaoAddress address) throws IOException {
         String aircraft = getClass().getResource("/aircraft.zip").getFile();
-        String stringLineFiltered = getStringLine(aircraft);
-        int index;
+        String stringLineFiltered = getStringLineFiltered(aircraft);
+        AircraftRegistration registration;
+        AircraftTypeDesignator designator;
+        String model;
+        AircraftDescription description;
+        WakeTurbulenceCategory wakeTurbulenceCategory;
 
         // up to here code works duh
         String addressString = String.valueOf(address);
-        if (stringLineFiltered.startsWith(addressString)) {
-            //finding the index of where the addressString is located
-            index = stringLineFiltered.indexOf(addressString);
 
-            // splits the string into an array for every line
-            String[] lines = stringLineFiltered.split(System.getProperty("line.separator"));
+        if(addressString.length() != 6){
+            return null;
         }
 
 
-        // in here you need to go through the stringLineFiltered and find the address given
-        /**
-         *Example of a few lines in stringLineFiltered:
-         *
-         * E48414,PR-GGU,B738,BOEING 737-800,L2J,M
-         * E48614,PR-GUF,B738,BOEING 737-800,L2J,M
-         * E48714,PP-JMJ,C750,CESSNA 750 Citation 10,L2J,M
-         * E48A14,PT-STK,C525,CESSNA 525 CitationJet,L2J,L
-         *
-         * the first value is the ICAO-address
-         */
+        // splits the string into an array for every line and comma
+        String[] lines = stringLineFiltered.split(System.getProperty("line.separator"));
 
+        // +6 because there are always 5 commas and one line.separator till the next ICAO
+        for (int i = 0; i < lines.length; i = i + 6) {
+            if(lines[i].startsWith(addressString)) {
 
-        //if(address == a certain icaoaddress)
+                registration = new AircraftRegistration(lines[i+1]);
+                designator = new AircraftTypeDesignator(lines[i+2]);
+                model = lines[i+3];
+                description = new AircraftDescription(lines[i+4]);
+                wakeTurbulenceCategory = WakeTurbulenceCategory.of(lines[i+5]);
+                return new AircraftData(registration, designator, model, description, wakeTurbulenceCategory);
+            }
 
-        return // has to be the address file called by icao;
-    }
+        }
 
-    private static String getStringLine(String aircraft){
+        return null; // has to be the address file called by icao;
+}
+
+    private static String getStringLineFiltered(String aircraft) {
 
         try (ZipFile zipFileUsed = new ZipFile(aircraft);
              InputStream stream = zipFileUsed.getInputStream(zipFileUsed.getEntry("14.csv"));
@@ -59,7 +63,7 @@ public final class AircraftDatabase {
                 System.out.println(stringLineFiltered);
             return stringLineFiltered;
         } catch (IOException e) {
-            throw new RuntimeException(e +" zipFileError");
+            throw new RuntimeException(e + " zipFileError");
         }
     }
 
