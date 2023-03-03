a0ce7015502c068767674e45049ee8327a17ea66
Dearop <rocamar564p@gmail.com>
2023-03-03 16:51:45 +0100

    Upload step 3 1

--- Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
+++ Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -1,10 +1,24 @@
 package ch.epfl.javions.demodulation;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public final class PowerComputer {
+    private int batchSize;
+    private short[] output = new short[batchSize];
+    private InputStream stream;
+    private  SamplesDecoder decoder;
+    public PowerComputer(InputStream stream, int batchSize){
+        if(batchSize % 8 != 0) throw new IllegalArgumentException();
+        this.batchSize = batchSize;
+        this.stream = stream;
+        decoder = new SamplesDecoder(stream, batchSize);
+    }
 
-    public PowerComputer(InputStream stream, int batchsize){
-        if(batchsize%8 != 0) throw new IllegalArgumentException();
+    public int readBatch(int[] batch) throws IOException{
+        int numberOfBatches = decoder.readBatch(output);
+        for(int i = 7; i < batchSize; i+=2){
+
+        }
     }
 }
