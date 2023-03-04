5c8382ccfead264d84a89055fb5f3da38c4e7997
Dearop <rocamar564p@gmail.com>
2023-03-04 16:46:33 +0100

    Decoder update

--- Javions/src/ch/epfl/javions/demodulation/SamplesDecoder.java
+++ Javions/src/ch/epfl/javions/demodulation/SamplesDecoder.java
@@ -8,29 +8,29 @@ import java.io.InputStream;
 public final class SamplesDecoder {
     private InputStream stream;
     private static int batchSize;
-    private byte[] bytes = new byte[2*batchSize];
-    public short[] batch = new short[batchSize];
+    private byte[] bytes;
+    public short[] batch;
 
     public SamplesDecoder(InputStream stream, int batchSize) {
         if (batchSize <= 0) throw new IllegalArgumentException();
         if(stream == null) throw new NullPointerException();
         this.stream = stream;
         this.batchSize = batchSize;
+        bytes = new byte[2*batchSize];
+        batch = new short[batchSize];
     }
 
     public int readBatch(short[] batch) throws IOException{
         Preconditions.checkArgument(batch.length == batchSize);
         if(stream.readAllBytes().length <= batchSize) return (int) Math.floor(stream.readAllBytes().length/2);
-        bytes = stream.readNBytes(batchSize);
-        int counter = 0;
+        int streamSize = stream.readNBytes(bytes, 0,batchSize);
         for(int i = 0; i < bytes.length/2; i+=2){
             short lowerWeight = (short) (bytes[i]-2048);
             short higherWeight = (short) (Bits.extractUInt(bytes[i+1], 4, 4)-2048);
             higherWeight <<= 8;
             batch[i/2] = (short) (higherWeight | lowerWeight);
-            ++counter;
         }
         this.batch = batch.clone();
-        return counter;
+        return streamSize/2;
     }
 }
--- Javions/test/ch/epfl/javions/SamplesDecoderTest.java
+++ Javions/test/ch/epfl/javions/SamplesDecoderTest.java
@@ -4,6 +4,8 @@ import ch.epfl.javions.demodulation.SamplesDecoder;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,14 +23,30 @@ public class SamplesDecoderTest {
     public void SamplesDecoderDoesWhatsExpected() throws IOException {
 
         String samples = getClass().getResource("/samples.bin").getFile();
+        samples = URLDecoder.decode(samples, StandardCharsets.UTF_8);
         InputStream stream = new FileInputStream(samples);
 
-        SamplesDecoder decoder = new SamplesDecoder(stream, 10);
-        short[] batch = new short[10];
-        assertEquals(10, decoder.readBatch(batch));
-
-
+        SamplesDecoder decoder = new SamplesDecoder(stream, 2402);
+        short[] batch = new short[2402];
+        System.out.println(decoder.readBatch(batch));
+        assertEquals(1201, decoder.readBatch(batch));
     }
 
+    @Test
+    void testValidSampleDecoder() throws IOException {
+        String stream2 = getClass().getResource("/samples.bin").getFile();
+        stream2 = URLDecoder.decode(stream2, StandardCharsets.UTF_8);
+        InputStream stream = new FileInputStream(stream2);
+        SamplesDecoder test = new SamplesDecoder(stream, 2402);
+        //  PowerComputer test2 = new PowerComputer(stream , 9608);
 
+        short[] batch = new short[2402];
+        test.readBatch(batch);
+        //  int [] batch2 = new int[9608];
+        // int a = test2.readBatch(batch2);
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(batch[i]);
+        }
+
+    }
 }
