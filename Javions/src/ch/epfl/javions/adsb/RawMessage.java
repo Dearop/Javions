909991dba4899a9e0df0dcb939eb0a9f4a55abf6
Dearop <rocamar564p@gmail.com>
2023-03-16 11:42:54 +0100

    latest

--- Javions/src/ch/epfl/javions/adsb/RawMessage.java
+++ Javions/src/ch/epfl/javions/adsb/RawMessage.java
@@ -7,6 +7,7 @@ import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
 /**
+ * Record representing a message in its raw form before extraction of the different parts of the message.
  *
  * @param timeStampNs
  * @param bytes representing the bytes that represent the messages
@@ -16,7 +17,9 @@ public record RawMessage(long timeStampNs, ByteString bytes) {
     private final static int CAStart= 0;
     private final static int CASize = 3;
     private final static int DFSize = 5;
-    private final static int timeBetweenTwoBatches = 120000;
+    private final static int DFLocation = 0;
+    private final static int ExpectedDF = 17;
+    private static Crc24 crc = new Crc24(Crc24.GENERATOR);
 
     public RawMessage(long timeStampNs, ByteString bytes){
         Preconditions.checkArgument(timeStampNs > 0 && LENGTH == bytes.size());
@@ -24,15 +27,33 @@ public record RawMessage(long timeStampNs, ByteString bytes) {
         this.bytes = bytes;
     }
 
+    /**
+     *
+     * @param timeStampNs long value representing the time from the reception of the ads-b waves to the time of
+     *                    processing the current instance of RawMessage.
+     * @param bytes byte table containing the message.
+     * @return RawMessage with the parameters given to the function if the crc of the message is 0 or null otherwise
+     */
     public static RawMessage of(long timeStampNs, byte[] bytes){
-        Crc24 crc = new Crc24(Crc24.GENERATOR);
-        if(crc.crc(bytes) != 0) return null;
-        timeStampNs += 120000;
+        byte[] crcByte = new byte[3];
+        // getting the 3 last bytes to check for crc TODO this probably does not work we only get 3 crc=0 when there should be 384
+        for (int i = 0; i < 3; i++) {
+            crcByte[i] = bytes[i+11];
+        }
+        //System.out.println(crc.crc(crcByte));
+        if(crc.crc(crcByte) != 0) return null;
+
         return new RawMessage(timeStampNs, new ByteString(bytes));
     }
 
+    /**
+     * @param byte0 first byte of the message
+     * @return integer value representing either the length of the message if the Down Link Format
+     * is the expected value (17) or 0 if it isn't
+     */
     public static int size(byte byte0){
-        if(Bits.extractUInt(byte0,CAStart + CASize,DFSize) == 17) return LENGTH;
+        byte shiftedByte = (byte) (byte0 >> 3);
+        if(Byte.toUnsignedInt(shiftedByte) != ExpectedDF) return LENGTH;
         return 0;
     }
 
@@ -40,10 +61,17 @@ public record RawMessage(long timeStampNs, ByteString bytes) {
         return (int) (payload >> 51);
     }
 
+    /**
+     * @return integer value representing the DF format of the message
+     */
     public int downLinkFormat(){
-        return bytes.byteAt(0) >> 3;
+        return bytes.byteAt(DFLocation) >> CASize;
     }
 
+    /**
+     *
+     * @return Icao address contained in the message
+     */
     public IcaoAddress icaoAddress(){
         byte[] byteAddress = new byte[3];
         for(int i = 1; i < 4; ++i){
