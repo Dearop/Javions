1d953c4acdc5f95c7e5c12304550d424181bf356
Dearop <rocamar564p@gmail.com>
2023-03-19 12:06:26 +0100

    some changes

--- Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -1,5 +1,6 @@
 package ch.epfl.javions.adsb;
 
+import ch.epfl.javions.Bits;
 import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
@@ -21,25 +22,28 @@ public record AircraftIdentificationMessage
 
     public static AircraftIdentificationMessage of(RawMessage rawMessage){
         //computing the category
-        int MSB = (14 - rawMessage.typeCode()) << 4;
-        int LSB = rawMessage.bytes().byteAt(4) & 0b111;
-        int category = MSB | LSB;
+        byte MSB = (byte) ((14 - rawMessage.typeCode()) << 4);
+        byte LSB = (byte) (rawMessage.bytes().byteAt(4) & 0b111);
+        int category = Byte.toUnsignedInt((byte) (MSB | LSB));
         //computing the CallSign
         StringBuilder sign = new StringBuilder();
         char intermediary = '\0';
         long payload = rawMessage.payload();
         int b;
+
         for (int i = 42; i >= 0; i-=6) {
-            b =  (int) (payload >> i) & 127;
+            b = Bits.extractUInt(payload, i, 6);
             if(!isValidCharCode(b)) return null;
-            System.out.println(b);
             if(b >= 48)intermediary = (char) ((b-48)+'0');
             if(b <= 26) intermediary = (char) (b+64);
             if(b == 32) intermediary = 32;
             sign.append(intermediary);
-            }
-        CallSign callSign1 = new CallSign(sign.toString());
+        }
+
+        String finishedSign = sign.toString();
+        CallSign callSign1 = new CallSign(finishedSign.stripTrailing());
         return new AircraftIdentificationMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), category, callSign1);
+        // I'm getting too many I think
     }
 
     private static boolean isValidCharCode(int code) {
--- Javions/test/ch/epfl/javions/adsb/MyAirbornePositionMessage.java
+++ Javions/test/ch/epfl/javions/adsb/MyAirbornePositionMessage.java
@@ -8,21 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
-public class MyAircraftIdentificationMessageTest {
+public class MyAirbornePositionMessage {
     private final String f = "C:\\Users\\Paul\\Dropbox\\PC\\Documents\\EPFL\\BA 2\\POOP\\Javions\\Javions\\Javions\\resources\\samples_20230304_1442.bin";
     @Test
-    public void AircraftIdentificationMessagePrintsFirstMessageRight() throws IOException {
+    public void PrintFirstAibornePositionMessage() throws IOException {
         try (
                 InputStream s = new FileInputStream(f)) {
                 AdsbDemodulator d = new AdsbDemodulator(s);
                 RawMessage m;
                 while ((m = d.nextMessage()) != null) {
-                    if(AircraftIdentificationMessage.of(m) != null){
-                        System.out.println(AircraftIdentificationMessage.of(m));
+                    if (AirbornePositionMessage.of(m) != null) {
+                        System.out.println(AirbornePositionMessage.of(m));
                     }
                 }
         }
     }
 }
-
-
