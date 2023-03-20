983db2768a1745af99b0949cfc1a42353bb220ba
Dearop <rocamar564p@gmail.com>
2023-03-20 21:37:33 +0100

    kinda done

--- Javions/test/ch/epfl/javions/adsb/MyCprDecoderTest.java
+++ Javions/test/ch/epfl/javions/adsb/MyCprDecoderTest.java
@@ -18,4 +18,15 @@ public class MyCprDecoderTest {
                         (int) Units.convert(46.323349, Units.Angle.DEGREE, Units.Angle.T32)));
     }
 
+    @Test
+    public void CprDecoderWorksWithGivenExample2() {
+        double x0 = 111600 / Math.pow(2, 17);
+        double x1 = 108865 / Math.pow(2, 17);
+        double y0 = 94445 / Math.pow(2, 17);
+        double y1 = 77558 / Math.pow(2, 17);
+        GeoPos expected = new GeoPos((int) Units.convert(7.476062, Units.Angle.DEGREE, Units.Angle.T32),
+                (int) Units.convert(46.323349, Units.Angle.DEGREE, Units.Angle.T32));
+        GeoPos actual = CprDecoder.decodePosition(x0, y0, x1, y1, 0);
+        assertEquals(expected, actual);
+    }
 }
