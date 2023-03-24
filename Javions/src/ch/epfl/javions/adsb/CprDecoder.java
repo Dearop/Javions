dfecf1de785a191113695101d511d8b1a92ff21c
Dearop <rocamar564p@gmail.com>
2023-03-24 16:06:32 +0100

    Wheeeeeeeeeeeeeeeee

--- Javions/src/ch/epfl/javions/adsb/CprDecoder.java
+++ Javions/src/ch/epfl/javions/adsb/CprDecoder.java
@@ -3,18 +3,30 @@ package ch.epfl.javions.adsb;
 import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.Units;
+/**
+ * This class decodes the geographic position from two local positions, x0 and y0 representing the even message
+ * and x1 and y1 representing the odd message. It also takes an integer value of mostRecent to tell us which message
+ * is the most recent.
+ *
+ * @author Paul Quesnot (347572)
+ */
 
 public class CprDecoder {
     private CprDecoder(){}
 
     /**
+     * Decodes the geographic position based on the provided parameters.
+     * The method first checks that the mostRecent value is either 0 or 1, then calculates the
+     * variables A0 and A1. It then does a check to see if the even and odd zone locations are not equal and returns null.
+     * Otherwise, it calculates the longitude and latitude values and converts them to t32 units. Finally,
+     * it returns the geographic position as a GeoPos object.
      *
-     * @param x0 double value representing the local longitude of the even message
-     * @param y0 double value representing the local latitude of the even message
-     * @param x1 double value representing the local longitude of the odd message
-     * @param y1 double value representing the local latitude of the odd message
+     * @param x0         double value representing the local longitude of the even message
+     * @param y0         double value representing the local latitude of the even message
+     * @param x1         double value representing the local longitude of the odd message
+     * @param y1         double value representing the local latitude of the odd message
      * @param mostRecent integer value letting us know which message is the most recent (odd or even)
-     * @return the latest geographic position
+     * @return the latest geographic position as a GeoPos object or null if decoding failed
      */
     public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent){
         Preconditions.checkArgument((mostRecent == 0) || (mostRecent == 1));
@@ -57,10 +69,25 @@ public class CprDecoder {
         return new GeoPos((int) Math.rint(actualLongT32), (int) Math.rint(actualLatT32));
     }
 
+    /**
+     * This method calculates the angles, that we need to calculate the A0 and A1 value.
+     *
+     * @param numberOfZones We split earth into 60 and 59 zones, with that we can calculate a more precise position of the aircraft.
+     * @param currentAngle  Is either Phi-even or Phi-odd
+     * @return returns the arccos of formula seen in the course.
+     */
     private static double AngleToZoneCalculator(double numberOfZones, double currentAngle){
         return Math.acos(1-((1-Math.cos(2*Math.PI/numberOfZones))/Math.pow(Math.cos(currentAngle),2)));
     }
 
+    /**
+     * This method returns the Phi or Lambda values, these will then be used in the AngleToZoneCalculator method.
+     *
+     * @param numberOfZones We split earth into 60 and 59 zones, with that we can calculate a more precise position of the aircraft.
+     * @param currentZone Current zone in which the aircraft is located.
+     * @param position position that is given in the parameter of the method decodePosition.
+     * @return return Phi0, Phi1, Lambda0 and Lambda1 depending on the parameters.
+     */
     private static double currentZone(double numberOfZones, double currentZone, double position){
         currentZone = (currentZone < 0) ? (currentZone + numberOfZones) : currentZone;
         double angle = (currentZone + position)/ numberOfZones;
