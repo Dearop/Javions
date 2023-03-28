cef79a708baa4c653d000c08796f12727ebad37e
Dearop <rocamar564p@gmail.com>
2023-03-28 09:34:32 +0200

    Bonjourno

--- Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
@@ -32,22 +32,30 @@ public class AircraftStateAccumulator<T extends AircraftStateSetter> {
                     latestAccordingMessage = latestOddMessage;
                 }
                 latestAccordingMessage = latestEvenMessage;
-                if (message.timeStampNs() - latestAccordingMessage.timeStampNs() >= 1E10 &&
-                        latestAccordingMessage.parity() != apm.parity()) {
-                    double x0, y0, x1, y1;
-                    if (apm.parity() == 0) {
-                        x0 = apm.x();
-                        y0 = apm.y();
-                        y1 = latestAccordingMessage.x();
-                        x1 = latestAccordingMessage.y();
-                        latestEvenMessage = apm;
+                System.out.println("coc");
+                if(latestAccordingMessage != null){
+                    System.out.println("coc in");
+                    if (message.timeStampNs() - latestAccordingMessage.timeStampNs() >= 1E10 &&
+                            latestAccordingMessage.parity() != apm.parity()) {
+                        System.out.println("coc out");
+                        double x0, y0, x1, y1;
+                        if (apm.parity() == 0) {
+                            x0 = apm.x();
+                            y0 = apm.y();
+                            y1 = latestAccordingMessage.x();
+                            x1 = latestAccordingMessage.y();
+                        }else {
+                            x0 = latestAccordingMessage.x();
+                            y0 = latestAccordingMessage.y();
+                            x1 = apm.x();
+                            y1 = apm.y();
+                        }
+                        if(apm.parity() == 0){
+                            latestAccordingMessage = apm;
+                        }
+                        latestAccordingMessage = apm;
+                        stateSetter.setPosition(CprDecoder.decodePosition(x0, y0, x1, y1, apm.parity()));
                     }
-                    x0 = latestAccordingMessage.x();
-                    y0 = latestAccordingMessage.y();
-                    x1 = apm.x();
-                    y1 = apm.y();
-                    latestOddMessage = apm;
-                    stateSetter.setPosition(CprDecoder.decodePosition(x0, y0, x1, y1, apm.parity()));
                 }
             }
             default -> throw new NullPointerException();
