0da567a79f81140c1de7909902cde62160b30ad2
Dearop <rocamar564p@gmail.com>
2023-04-25 11:19:53 +0200

    incredible

--- Javions/src/ch/epfl/javions/gui/BaseMapController.java
+++ Javions/src/ch/epfl/javions/gui/BaseMapController.java
@@ -53,19 +53,32 @@ public final class BaseMapController {
 
         GraphicsContext graphics = canvas.getGraphicsContext2D();
 
+        System.out.println(mapPane.getWidth());
+
         int minTileX = tilePositionCalculator(parameter.getMinX());
         int maxTileX = tilePositionCalculator(parameter.getMinX() + mapPane.getWidth());
-        System.out.println(mapPane.getWidth());
-        int minTileY = tilePositionCalculator(parameter.getMinY());
-        int maxTileY = tilePositionCalculator(parameter.getMinY() + mapPane.getHeight());
 
-        System.out.printf("%d->%d, %d->%d", minTileX, maxTileX, minTileY, maxTileY);
+        int minTileY = tilePositionCalculator(parameter.getMinY());
+        int maxTileY = tilePositionCalculator(parameter.getMinY() + mapPane.getWidth());
+        System.out.println(parameter.getZoom()+"zooooom");
+
+
+        int windowX = 0;
+        int windowY = 0;
         for (int y = minTileY; y < maxTileY; y++) {
+            windowY = (y == minTileY) ? 0 : windowY + 256;
             for (int x = minTileX; x < maxTileX; x++) {
+                windowX = (x == minTileX) ? 0 : windowX + 256;
+//                if (viewPositionX >= 0 || viewPositionX <= Math.scalb(1, (int) (8 + currentZoomLevel)) &&
+//                        viewPositionY >= 0 || viewPositionX <= Math.scalb(1, (int) (8 + currentZoomLevel)))
                 try {
                     Image image = tileManager.imageForTileAt(new TileManager.TileId(parameter.getZoom(), x, y));
-                    graphics.drawImage(image, x * TILE_SIZE, y * TILE_SIZE);
-                } catch (IOException ignored) {}
+                    graphics.drawImage(image, windowX, windowY);
+                    System.out.println(x + "x and " + y + " y");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                //}
             }
         }
     }
