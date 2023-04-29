b57874095ccd46cb9c1ce4e514eaa433096ebd9c
Dearop <rocamar564p@gmail.com>
2023-04-29 11:26:00 +0200

    slowly dying

--- Javions/src/ch/epfl/javions/gui/AircraftController.java
+++ Javions/src/ch/epfl/javions/gui/AircraftController.java
@@ -4,6 +4,7 @@ import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.WebMercator;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.*;
+import javafx.beans.value.ObservableDoubleValue;
 import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
 import javafx.scene.Group;
@@ -19,6 +20,7 @@ public final class AircraftController {
     private ObservableSet<ObservableAircraftState> knownStates;
     private ObjectProperty<ObservableAircraftState> currentSelectedState;
     private Pane aircraftPane;
+    private SimpleBooleanProperty airCraftClicked;
     private static final int ALTITUDE_CEILING = 12000;
     public AircraftController(MapParameters parameters, ObservableSet<ObservableAircraftState> knownStates,
                               ObjectProperty<ObservableAircraftState> currentSelectedState){
@@ -40,22 +42,11 @@ public final class AircraftController {
     }
 
     private Group individualAircraftGroup(ObservableAircraftState oas){
-        Group iconAndLabel = new Group(new Group(aircraftLabel(oas), new Group(aircraftIcon(oas))));
-        //binding the icon and label to the position of the aircraft
-        SimpleObjectProperty<GeoPos> position = new SimpleObjectProperty<>();
-        //bind the position of the aircraft to the position we are using
-        position.bind(oas.positionProperty());
-       double positionX = WebMercator.x(parameters.getZoom(), position.get().latitude());
-        double positionY = WebMercator.y(parameters.getZoom(), position.get().longitude());
-        // lmk what you think of this if statement, basically I'm only drawing the labels on the aircraft that we can see on the screen
-        // TODO: 4/29/2023: might have to add a listener that observes the pane here if we don't see anything
-        if(parameters.getMinX() < positionX && positionX < parameters.getMinX() + aircraftPane.getWidth()
-                && parameters.getMinY() < positionY && positionY < parameters.getMinY() + aircraftPane.getHeight()){
-            DoubleProperty positionOnPaneX = new SimpleDoubleProperty(parameters.getMinX() - positionX);
-            DoubleProperty positionOnPaneY = new SimpleDoubleProperty(parameters.getMinY() - positionY);
-            iconAndLabel.layoutXProperty().bind(positionOnPaneX);
-            iconAndLabel.layoutYProperty().bind(positionOnPaneY);
-        }
+        Group label = new Group(aircraftLabelInitialisation(oas));
+        Group icon = new Group(aircraftIconInitialisation(oas));
+        label.visibleProperty().bind(airCraftClicked);
+        Group iconAndLabel = new Group(label, icon);
+        aircraftLabelAndIconPositioning(oas, iconAndLabel);
         Group aircraftGroup =
                 new Group(aircraftTrajectory(oas), iconAndLabel);
         aircraftGroup.setId(oas.icaoAddress().string());
@@ -73,7 +64,26 @@ public final class AircraftController {
         return null;
     }
 
-    public Group aircraftLabel(ObservableAircraftState oas){
+
+    private void aircraftLabelAndIconPositioning(ObservableAircraftState oas, Group iconAndLabel){
+        //binding the icon and label to the position of the aircraft
+        SimpleObjectProperty<GeoPos> position = new SimpleObjectProperty<>();
+        //bind the position of the aircraft to the position we are using
+        position.bind(oas.positionProperty());
+        double positionX = WebMercator.x(parameters.getZoom(), position.get().latitude());
+        double positionY = WebMercator.y(parameters.getZoom(), position.get().longitude());
+        // lmk what you think of this if statement, basically I'm only drawing the labels on the aircraft that we can see on the screen
+        // TODO: 4/29/2023: might have to add a listener that observes the pane here if we don't see anything
+        if(parameters.getMinX() < positionX && positionX < parameters.getMinX() + aircraftPane.getWidth()
+                && parameters.getMinY() < positionY && positionY < parameters.getMinY() + aircraftPane.getHeight()){
+           iconAndLabel.layoutXProperty().bind(
+                    Bindings.createDoubleBinding(() -> parameters.getMinX() - positionX));
+           iconAndLabel.layoutYProperty().bind(
+                    Bindings.createDoubleBinding(() -> parameters.getMinY() - positionY));
+        }
+    }
+
+    public Group aircraftLabelInitialisation(ObservableAircraftState oas){
         Rectangle rectangle = new Rectangle();
         Text text = new Text();
         text.textProperty().bind(
@@ -84,6 +94,8 @@ public final class AircraftController {
         text.textProperty().bind(
                 Bindings.createStringBinding(() ->
                         String.format("%f mètres", oas.getAltitude()), oas.altitudeProperty()));
+        rectangle.heightProperty().bind(
+                text.layoutBoundsProperty().map(b -> b.getHeight()));
         rectangle.widthProperty().bind(
                 text.layoutBoundsProperty().map(b -> b.getWidth() + 4));
         Group label = new Group(rectangle, text);
@@ -91,7 +103,7 @@ public final class AircraftController {
         return label;
     }
 
-    private Group aircraftIcon(ObservableAircraftState oas){
+    private Group aircraftIconInitialisation(ObservableAircraftState oas){
         AircraftIcon icon = AircraftIcon.iconFor(oas.getData().typeDesignator(), oas.getData().description(),
                 oas.getCategory(), oas.getData().wakeTurbulenceCategory());
         SVGPath aircraftIcon = new SVGPath();
@@ -116,4 +128,8 @@ public final class AircraftController {
            aircraftPane.getChildren().remove(individualAircraftGroup(c.getElementRemoved()));
         });
     }
+
+    private void isShowable(Group label, Group icon){
+    }
+
 }
